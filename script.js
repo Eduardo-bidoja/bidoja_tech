6f533f5efb0318733b773d0f936496bc9abcb9c9
Eduardo Bidoja <119968514+Eduardo-bidoja@users.noreply.github.com>
2025-07-03 11:06:34 +0100

    Update script.js
    resolvido o problema

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('updateaprendizados.json')
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById('aprendizados');
-    data.semanas.forEach(item => {
+ data.week.forEach(item => {
       const div = document.createElement('div');
       div.className = 'aprendizado';
       div.innerHTML = `<h3>${item.titulo}</h3><p>${item.conteudo}</p>`;
